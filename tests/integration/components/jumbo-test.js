6a8d44bae6ec45de75f25006acf3f2b88829a2fb
Denis Toledo <contato@denistoledo.com.br>
2020-02-06 14:48:36 -0300

    Add components

---
+++ tests/integration/components/jumbo-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | jumbo', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the content inside a jumbo header with a tomster', async function(assert) {
+    await render(hbs`
+      <Jumbo>
+        template block text
+      </Jumbo>
+    `);
+
+    assert.dom('.jumbo').exists();
+    assert.dom('.jumbo').hasText('template block text');
+    assert.dom('.jumbo .tomster').exists();
+  });
+});
